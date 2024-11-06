88b00b1fe4c5114015a7a5c4a8ad78cd8bb11b0a
Jose Carlos Jimenez <jimenez.jcarlos28@gmail.com>
2024-11-05 17:37:07 -0700

    funcionalidad completa sin bd

--- lib/pages/add_event_page.dart
+++ lib/pages/add_event_page.dart
@@ -1,51 +1,286 @@
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:excel/excel.dart';
+
+// class AddEventPage extends StatefulWidget {
+//   final Function(Map<String, dynamic>) onAddEvent;
+
+//   const AddEventPage({super.key, required this.onAddEvent});
+
+//   @override
+//   _AddEventPageState createState() => _AddEventPageState();
+// }
+
+// class _AddEventPageState extends State<AddEventPage> {
+//   final TextEditingController _eventDateController = TextEditingController();
+//   final TextEditingController _guestCountController = TextEditingController();
+//   final TextEditingController _startTimeController = TextEditingController();
+//   List<Map<String, dynamic>> guests = [];
+//   File? excelFile;
+
+//   void _pickExcelFile() async {
+//     final result = await FilePicker.platform
+//         .pickFiles(type: FileType.custom, allowedExtensions: ['xlsx']);
+//     if (result != null) {
+//       setState(() {
+//         excelFile = File(result.files.single.path!);
+//       });
+//       _loadGuestsFromExcel(excelFile!);
+//     }
+//   }
+
+//   void _loadGuestsFromExcel(File file) async {
+//     final bytes = file.readAsBytesSync();
+//     final excel = Excel.decodeBytes(bytes);
+
+//     // Ajustar la función para leer las columnas de tu archivo
+//     for (var table in excel.tables.keys) {
+//       final sheet = excel.tables[table];
+//       if (sheet != null) {
+//         for (var row in sheet.rows.skip(1)) {
+//           setState(() {
+//             guests.add({
+//               "name": row[0]?.value.toString(), // Nombre
+//               "table": row[1]?.value.toString(), // Mesa
+//               "peopleCount": row[2]?.value.toString(), // Personas
+//               "invitedBy": row[3]?.value.toString(), // Invitado por
+//               "confirmed": row[4]?.value.toString().toLowerCase() == "si", // Confirmó asistencia
+//               "scanned": row[5]?.value.toString().toLowerCase() == "si", // Asistencia
+//               "timeScanned": row[6]?.value.toString()
+//             });
+//           });
+//         }
+//         print(guests);
+//       }
+//     }
+//   }
+
+//   void _addEvent() {
+//     if (_eventDateController.text.isNotEmpty && excelFile != null) {
+//       widget.onAddEvent({
+//         "name": excelFile!.path.split('/').last.split('.').first,
+//         "date": _eventDateController.text,
+//         "guestCount": _guestCountController.text,
+//         "startTime": _startTimeController.text,
+//         "guests": guests,
+//       });
+//       Navigator.pop(context);
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Por favor completa todos los campos')),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Agregar Evento')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             TextField(
+//               controller: _eventDateController,
+//               decoration: const InputDecoration(labelText: 'Fecha del Evento'),
+//             ),
+//             TextField(
+//               controller: _guestCountController,
+//               decoration:
+//                   const InputDecoration(labelText: 'Cantidad de Personas'),
+//               keyboardType: TextInputType.number,
+//             ),
+//             TextField(
+//               controller: _startTimeController,
+//               decoration:
+//                   const InputDecoration(labelText: 'Hora de Inicio del Evento'),
+//             ),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: _pickExcelFile,
+//               child: const Text('Añadir desde archivo Excel'),
+//             ),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: _addEvent,
+//               child: const Text('Agregar Evento'),
+//             ),
+//             const SizedBox(height: 20),
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: guests.length,
+//                 itemBuilder: (context, index) {
+//                   return ListTile(
+//                     title: Text(guests[index]['name']),
+//                     subtitle: Text(guests[index]['confirmed']
+//                         ? 'si'
+//                         : 'no'),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:excel/excel.dart';
 
-class AddEventPage extends StatelessWidget {
-  const AddEventPage({super.key});
+class AddEventPage extends StatefulWidget {
+  final Function(Map<String, dynamic>) onAddEvent;
 
-  Future<void> _pickExcelFile(BuildContext context) async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['xlsx'],
-    );
+  const AddEventPage({super.key, required this.onAddEvent});
 
+  @override
+  _AddEventPageState createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+  final TextEditingController _eventDateController = TextEditingController();
+  final TextEditingController _guestCountController = TextEditingController();
+  final TextEditingController _startTimeController = TextEditingController();
+  List<Map<String, dynamic>> guests = [];
+  File? excelFile;
+
+  void _pickExcelFile() async {
+    final result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['xlsx']);
     if (result != null) {
-      var bytes = result.files.single.bytes;
-      var excel = Excel.decodeBytes(bytes!);
+      setState(() {
+        excelFile = File(result.files.single.path!);
+      });
+      _loadGuestsFromExcel(excelFile!);
+    }
+  }
 
-      List<Map<String, dynamic>> guests = [];
+  void _loadGuestsFromExcel(File file) async {
+    final bytes = file.readAsBytesSync();
+    final excel = Excel.decodeBytes(bytes);
 
-      for (var table in excel.tables.keys) {
-        for (var row in excel.tables[table]!.rows) {
-          if (row[0] != null && row[1] != null) {
+    for (var table in excel.tables.keys) {
+      final sheet = excel.tables[table];
+      if (sheet != null) {
+        for (var row in sheet.rows.skip(1)) {
+          setState(() {
             guests.add({
-              'name': row[0]!.value.toString(),
-              'confirmed': row[1]!.value.toString().toLowerCase() == 'sí',
-              'scanned': false,
-              'scanTime': null,
+              "name": row[0]?.value.toString(),
+              "table": row[1]?.value.toString(),
+              "peopleCount": row[2]?.value.toString(),
+              "invitedBy": row[3]?.value.toString(),
+              "confirmed": row[4]?.value.toString().toLowerCase() == "si",
+              "scanned": row[5]?.value.toString().toLowerCase() == "si",
+              "timeScanned": row[6]?.value.toString()
             });
-          }
+          });
         }
       }
+    }
+  }
 
-      Navigator.pop(context, {'guests': guests});
+  void _addEvent() {
+    if (_eventDateController.text.isNotEmpty && excelFile != null) {
+      widget.onAddEvent({
+        "name": excelFile!.path.split('/').last.split('.').first,
+        "date": _eventDateController.text,
+        "guestCount": _guestCountController.text,
+        "startTime": _startTimeController.text,
+        "guests": guests,
+      });
+      Navigator.pop(context);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Por favor completa todos los campos')),
+      );
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      setState(() {
+        _eventDateController.text = "${picked.toLocal()}".split(' ')[0];
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _startTimeController.text = picked.format(context);
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agregar Evento desde Excel'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            _pickExcelFile(context);
-          },
-          child: const Text('Seleccionar archivo de Excel'),
+      appBar: AppBar(title: const Text('Agregar Evento')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _eventDateController,
+              decoration: const InputDecoration(
+                labelText: 'Fecha del Evento',
+                hintText: 'Selecciona la fecha',
+              ),
+              readOnly: true,
+              onTap: () => _selectDate(context),
+            ),
+            TextField(
+              controller: _guestCountController,
+              decoration:
+                  const InputDecoration(labelText: 'Cantidad de Personas'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: _startTimeController,
+              decoration: const InputDecoration(
+                labelText: 'Hora de Inicio del Evento',
+                hintText: 'Selecciona la hora',
+              ),
+              readOnly: true,
+              onTap: () => _selectTime(context),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickExcelFile,
+              child: const Text('Añadir desde archivo Excel'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _addEvent,
+              child: const Text('Agregar Evento'),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: guests.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(guests[index]['name']),
+                    subtitle: Text(guests[index]['confirmed'] ? 'sí' : 'no'),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/scan_event_page.dart
+++ lib/pages/scan_event_page.dart
@@ -1,5 +1,101 @@
+// import 'package:flutter/material.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+// class ScanEventPage extends StatefulWidget {
+//   final Map<String, dynamic> event;
+
+//   const ScanEventPage({super.key, required this.event});
+
+//   @override
+//   _ScanEventPageState createState() => _ScanEventPageState();
+// }
+
+// class _ScanEventPageState extends State<ScanEventPage> {
+//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+//   QRViewController? controller;
+//   String? qrText;
+
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     if (controller != null) {
+//       controller!.pauseCamera();
+//       controller!.resumeCamera();
+//     }
+//   }
+
+//   void _onQRViewCreated(QRViewController controller) {
+//     this.controller = controller;
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         qrText = scanData.code;
+//       });
+//       _showScanResult(scanData.code);
+//     });
+//   }
+
+//   void _showScanResult(String? result) {
+//     if (result != null) {
+//       controller?.pauseCamera();
+//       showDialog(
+//         context: context,
+//         builder: (BuildContext context) {
+//           return AlertDialog(
+//             title: const Text('Código escaneado'),
+//             content: Text('Contenido: $result'),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop();
+//                   controller?.resumeCamera();
+//                 },
+//                 child: const Text('Aceptar'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Escanear Código QR'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 4,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Center(
+//               child: (qrText != null)
+//                   ? Text('Resultado: $qrText')
+//                   : const Text('Escanea un código QR'),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:intl/intl.dart';
 
 class ScanEventPage extends StatefulWidget {
   final Map<String, dynamic> event;
@@ -30,32 +126,65 @@ class _ScanEventPageState extends State<ScanEventPage> {
       setState(() {
         qrText = scanData.code;
       });
-      _showScanResult(scanData.code);
+      _processScanResult(scanData.code);
     });
   }
 
-  void _showScanResult(String? result) {
+  void _processScanResult(String? result) {
     if (result != null) {
+      final Map<String, dynamic>? scannedGuest = _findGuestInEvent(result);
+      String timeScanned = DateFormat('HH:mm').format(DateTime.now()).toString();
       controller?.pauseCamera();
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Código escaneado'),
-            content: Text('Contenido: $result'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  controller?.resumeCamera();
-                },
-                child: const Text('Aceptar'),
-              ),
-            ],
-          );
-        },
-      );
+      
+
+      if (scannedGuest != null) {
+        setState(() {
+          scannedGuest['scanned'] = true;
+          scannedGuest['timeScanned'] = timeScanned;
+        });
+        _showScanResultDialog(
+          title: 'Invitado encontrado',
+          content: 'Nombre: ${scannedGuest['name']}\nHora de escaneo: $timeScanned\nEstado: Escaneado',
+        );
+      } else {
+        _showScanResultDialog(
+          title: 'Invitado no encontrado',
+          content: 'El código QR pertenece a otro evento o es inválido.',
+        );
+      }
     }
+  }
+
+  Map<String, dynamic>? _findGuestInEvent(String guestId) {
+    // Busca el invitado en el evento actual usando el ID del QR
+    print("event " + widget.event.toString());
+    var guest = widget.event['guests']?.firstWhere(
+      (guest) => guest['name'] == guestId,
+      orElse: () => <String, dynamic>{},
+    );
+
+    return guest.isNotEmpty ? guest : null;
+  }
+
+  void _showScanResultDialog({required String title, required String content}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                controller?.resumeCamera();
+              },
+              child: const Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -83,10 +212,10 @@ class _ScanEventPageState extends State<ScanEventPage> {
             flex: 1,
             child: Center(
               child: (qrText != null)
-                  ? Text('Resultado: $qrText')
+                  ? Text('Resultado: $qrText["name"]')
                   : const Text('Escanea un código QR'),
             ),
-          )
+          ),
         ],
       ),
     );
